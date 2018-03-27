07e0403d72c8d53c413824afb5d9bfc6c7fa7dfc
Joseph Robinson <smartech56@gmail.com>
2018-03-27 14:02:26 -0700

    added git clone

--- compile.sh
+++ compile.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+mkdir Server
+mkdir Forge
+mkdir Proxy
+mkdir ServerPocketEdition
+mkdir plugins
+
+cd Server
 git clone https://github.com/GlowstoneMC/Glowstone.git
 git clone https://github.com/PaperMC/Paper.git
-git clone https://github.com/Nukkit/Nukkit.git
 git clone https://github.com/minecraftserverorg/cuberite.git
+git clone https://github.com/Diorite/Diorite.git
+git clone https://github.com/LanternPowered/LanternServer.git
+cd ..
+cd Forge
+git clone https://github.com/CyberdyneCC/Thermos.git
+cd ..
+cd Proxy
 git clone https://github.com/SpigotMC/BungeeCord.git
 git clone https://github.com/WaterfallMC/Travertine.git
+git clone https://github.com/WaterfallMC/Waterfall.git
+cd ..
+cd ServerPocketEdition
+git clone https://github.com/Nukkit/Nukkit.git
 git clone https://github.com/pmmp/PocketMine-MP.git
+git clone https://github.com/iTXTech/Genisys.git
+git clone https://github.com/PrismarineMC/Prismarine.git
+cd ..
+cd Plugins
+https://github.com/essentials/Essentials.git
