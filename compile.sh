83c05f0204c1046d7d56ecfdd58eacf6000de9e3
Joseph Robinson <smartech56@gmail.com>
2018-03-27 13:42:49 -0700

    added files from git

--- compile.sh
+++ compile.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+git clone https://github.com/GlowstoneMC/Glowstone.git
+git clone https://github.com/PaperMC/Paper.git
+git clone https://github.com/Nukkit/Nukkit.git
+git clone https://github.com/minecraftserverorg/cuberite.git
+git clone https://github.com/SpigotMC/BungeeCord.git
+git clone https://github.com/WaterfallMC/Travertine.git
+git clone https://github.com/pmmp/PocketMine-MP.git
